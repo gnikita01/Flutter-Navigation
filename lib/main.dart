34cc66c6666225e55ff140a2c1f3daf650341b81
nikitag01 <nikitag500@gmail.com>
2018-10-13 15:17:26 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(50.0), // here the desired height
+        child: AppBar(
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+            ),
+            IconButton(
+              icon: Icon(Icons.linked_camera),
+            ),
+          ],
+          // ...
+          title: Text('Scaffold Demo'),
+          backgroundColor: Colors.blue,
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                // Update the state of the app
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: new Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+          //  Image.asset('assets/test.png'),
+            Card(
+              child: new Column(
+                children: <Widget>[
+                  new Text(
+                    'I love Flutter',
+                    style: TextStyle(fontSize: 24.0),
+                  ),
+                  Image.network(
+                    'https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png',
+                    fit: BoxFit.fill,
+                  ),
+                ],
+              ),
+            ),
+            new Text(
+              'You have pushed the button this many times:',
+            ),
+            new Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+            RaisedButton(
+              child: Text('Launch screen'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondScreen()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: new Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: new BottomNavigationBar(
+        items: [
+          new BottomNavigationBarItem(
+            icon: new Icon(Icons.add),
+            title: new Text("Add"),
+          ),
+          new BottomNavigationBarItem(
+            icon: new Icon(Icons.edit),
+            title: new Text("Edit"),
+          ),
+          new BottomNavigationBarItem(
+            icon: new Icon(Icons.delete),
+            title: new Text("Delete"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Screen"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/modal.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+
+class ModalBottomSheetDemo extends StatelessWidget {
+  static const String routeName = '/material/modal-bottom-sheet';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Modal bottom sheet'),
+          //actions: <Widget>[MaterialDemoDocumentationButton(routeName)],
+        ),
+        body: Center(
+            child: RaisedButton(
+                child: const Text('SHOW BOTTOM SHEET'),
+                onPressed: () {
+                  showModalBottomSheet<void>(context: context, builder: (BuildContext context) {
+                    return Container(
+                        child: Padding(
+                            padding: const EdgeInsets.all(32.0),
+                            child: Text('This is the modal bottom sheet. Tap anywhere to dismiss.',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    color: Theme.of(context).accentColor,
+                                    fontSize: 24.0
+                                )
+                            )
+                        )
+                    );
+                  });
+                }
+            )
+        )
+    );
+  }
+}
